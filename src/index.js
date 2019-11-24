87540da4bff3115b8b2161236c12c5793c6cca89
aesarantsev <47385491+aesarantsev@users.noreply.github.com>
2019-11-24 12:07:53 +1000

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+// Import styles
+import style from "./styles/main.scss";
+import "./components/primitive-components/text-field/text-field.scss";
+import "./components/primitive-components/dropdown/dropdown.scss";
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+    dropwownScript: "./src/components/primitive-components/dropdown/script.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist")
+    //filename: "[name].js"
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: "babel-loader"
+      //   }
+      // },
+
+      {
+        test: /\.pug$/,
+        use: ["html-loader", "pug-html-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.pug",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
