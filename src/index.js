70692a63e96dcf837b3a38ad49b63dc46afb3020
aesarantsev <andrrr96@yandex.ru>
2019-11-30 01:41:01 +1000

    Правки Header, Footer, Buttons

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "./modules/primitive-modules/checkbox-item/checkbox-item";
 import "./modules/primitive-modules/checkbox-list/checkbox-list";
 import "./modules/primitive-modules/datepicker/datepicker";
 import "./modules/primitive-modules/dropdown/dropdown";
+import "./modules/header/header";
 
 // SCSS
 import "./main.scss";
