f986eae808b7620cf2f66561de5f6c33b618b818
aesarantsev <andrrr96@yandex.ru>
2019-11-30 14:20:44 +1000

    Правки Header. Заменил Favicon

---
+++ src/modules/header/header.js
@@ -0,0 +1,15 @@
+// $(function() {
+//   $(".nav__dropdown ul").hide();
+//   $(".nav__dropdown").hover(
+//     function() {
+//       $(this)
+//         .find("ul")
+//         .slideDown();
+//     },
+//     function() {
+//       $(this)
+//         .find("ul")
+//         .slideUp("fast");
+//     }
+//   );
+// });
