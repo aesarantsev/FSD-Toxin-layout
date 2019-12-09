765b91133ae133896ea177d950ea6e0189afe5f3
aesarantsev <andrrr96@yandex.ru>
2019-12-09 19:48:44 +1000

    Добавил BulletList

--- src/index.js
+++ src/index.js
@@ -12,5 +12,6 @@ import "air-datepicker";
 import "./blocks/primitive-blocks/dropdown/dropdown";
 import "./blocks/primitive-blocks/checkbox-list/checkbox-list";
 import "./blocks/primitive-blocks/radio-buttons/radio-button";
+import "./blocks/primitive-blocks/bullet-list/bullet-list";
 // SCSS
 import "./main.scss";
