0716b37bd36faefdb1020ce6db9e3e3113b6f8dd
aesarantsev <andrrr96@yandex.ru>
2019-12-12 13:17:16 +1000

    Вывел стрелочку в CheckboxList из Material Icons

--- src/blocks/primitive-blocks/checkbox-list/_expandable/checkbox-list_expandable.js
+++ src/blocks/primitive-blocks/checkbox-list/_expandable/checkbox-list_expandable.js
@@ -1,6 +1,9 @@
 //Classes
 let checkBoxListExpanableTitleClass = ".checkbox-list__title_expandable";
 let checkBoxListExpanableItemsClass = ".checkbox-list__items_expandable";
+let checkBoxListExpanableArrowClass = ".checkbox-list__title_expandable__arrow";
+let checkBoxListExpanableArrowReflectedClass =
+  ".checkbox-list__title_expandable__arrow_reflected";
 
 //Params
 let timeoutAnimation = 150;
@@ -12,6 +15,32 @@ jQuery(document).ready(function(e) {
       checkBoxListExpanableItemsClass,
       currentCheckBoxList
     );
+    let currentCheckBoxArrow = $(
+      checkBoxListExpanableArrowClass,
+      currentCheckBoxList
+    );
+
+    $(currentCheckBoxArrow).toggleClass(
+      checkBoxListExpanableArrowReflectedClass.slice(1),
+      checkBoxListExpanableArrowClass.slice(1)
+    );
+
+    // if (
+    //   $(currentCheckBoxArrow).hasClass(checkBoxListExpanableArrowClass.slice(1))
+    // ) {
+    //   $(currentCheckBoxArrow)
+    //     .removeClass(checkBoxListExpanableArrowClass.slice(1))
+    //     .addClass(checkBoxListExpanableArrowReflectedClass.slice(1));
+    // } else if (
+    //   $(currentCheckBoxArrow).hasClass(
+    //     checkBoxListExpanableArrowReflectedClass.slice(1)
+    //   )
+    // ) {
+    //   console.log("else");
+    //   $(currentCheckBoxArrow)
+    //     .removeClass(checkBoxListExpanableArrowReflectedClass.slice(1))
+    //     .addClass(checkBoxListExpanableArrowClass.slice(1));
+    // }
 
     if (currentCheckBoxItems.css("display") == "none")
       currentCheckBoxItems.show(timeoutAnimation);
