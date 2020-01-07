5ee985f820c7859b61c9a5abdf7d1cc3d4be7a4a
aesarantsev <andrrr96@yandex.ru>
2020-01-07 21:04:05 +1000

    refactor(checkboxListRich) #3

--- src/blocks/checkbox-item/checkbox-item.js
+++ src/blocks/checkbox-item/checkbox-item.js
@@ -1,17 +1,23 @@
 let checkBoxItemClass = ".checkbox-item";
+let checkBoxItemBoldClass = ".checkbox-item_bold";
 let checkBoxItemMarkClass = ".checkbox-item__mark";
 let checkBoxItemMarkCheckClass = ".checkbox-item__mark_check";
 
-$(checkBoxItemClass).click(function() {
-  if ($("input", this).attr("checked")) {
-    $("input", this).removeAttr("checked");
-    $("span", this)
-      .removeClass(checkBoxItemMarkCheckClass.slice(1))
-      .addClass(checkBoxItemMarkClass.slice(1));
+$([checkBoxItemClass, checkBoxItemBoldClass].join(", ")).click(function() {
+  let currentInput = $("input", this);
+  let currentSpan = $("span", this);
+
+  if (currentInput.attr("checked")) {
+    currentInput.removeAttr("checked");
+    currentSpan.toggleClass(
+      [checkBoxItemMarkCheckClass.slice(1), checkBoxItemMarkClass.slice(1)],
+      0
+    );
   } else {
-    $("input", this).attr("checked", "checked");
-    $("span", this)
-      .removeClass(checkBoxItemMarkClass.slice(1))
-      .addClass(checkBoxItemMarkCheckClass.slice(1));
+    currentInput.attr("checked", "checked");
+    currentSpan.toggleClass(
+      [checkBoxItemMarkCheckClass.slice(1), checkBoxItemMarkClass.slice(1)],
+      0
+    );
   }
 });
---
+++ src/blocks/checkbox-list-rich/checkbox-list-rich.js
@@ -0,0 +1,1 @@
+import "../checkbox-item/checkbox-item";
