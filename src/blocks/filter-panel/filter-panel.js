e5ddeb98949a3b6d798ad4fc7b5c38840f2fbb85
aesarantsev <andrrr96@yandex.ru>
2020-01-09 13:21:08 +1000

    refactor(Header) Adaptive header

--- src/blocks/filter-panel/filter-panel.js
+++ src/blocks/filter-panel/filter-panel.js
@@ -3,3 +3,10 @@ import "../dropdown/dropdown";
 import "../range-slider/range-slider";
 import "../checkbox-list/checkbox-list";
 import "../checkbox-list-rich/checkbox-list-rich";
+
+
+$(".filter-panel__toggle").click(function() {
+
+  $(".filter-panel__content").show(100);
+
+})
