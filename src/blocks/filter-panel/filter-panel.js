916524af9adfc565080b86d4b7193c69b15863e4
aesarantsev <andrrr96@yandex.ru>
2020-01-11 11:25:45 +1000

    refactor(FilterPanel) Adaptive FilterPanel

--- src/blocks/filter-panel/filter-panel.js
+++ src/blocks/filter-panel/filter-panel.js
@@ -4,9 +4,20 @@ import "../range-slider/range-slider";
 import "../checkbox-list/checkbox-list";
 import "../checkbox-list-rich/checkbox-list-rich";
 
+const filterPanelToggleElement = $(".filter-panel__toggle");
+const filterPanelContentElement = $(".filter-panel__content");
 
-$(".filter-panel__toggle").click(function() {
+filterPanelToggleElement.click(function() {
+  if (filterPanelContentElement.css("display") == "none")
+    filterPanelContentElement.show(100);
+  else filterPanelContentElement.hide(100);
 
-  $(".filter-panel__content").show(100);
+  $("body,html").animate({ scrollTop: 0 }, 400);
+});
 
-})
+function windowSize() {
+  if ($(window).width() > 982)
+  filterPanelContentElement.css("display", "block");
+}
+
+$(window).on("load resize", windowSize);
--- src/pages/searchRoom/searchRoom.js
+++ src/pages/searchRoom/searchRoom.js
@@ -1,5 +1,7 @@
 import "webpack-jquery-ui";
 import "webpack-jquery-ui/css";
+import "air-datepicker";
 
 import "../../blocks/filter-panel/filter-panel";
 import "../../blocks/room-list/room-list";
+import "../../blocks/datepicker/datepicker";
