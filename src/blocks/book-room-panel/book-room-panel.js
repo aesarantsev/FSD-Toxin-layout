bbd4864e7d170ee07d25c60c5908617004dda817
aesarantsev <andrrr96@yandex.ru>
2020-01-24 21:19:23 +1000

    refactor(bookRoomPanel) #3

--- src/blocks/book-room-panel/book-room-panel.js
+++ src/blocks/book-room-panel/book-room-panel.js
@@ -1,3 +1,4 @@
 import "../text-field/text-field";
 import "../dropdown/dropdown";
 import "../btn/btn";
+import "../info-button/info-button";
--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -3,4 +3,4 @@ import "air-datepicker";
 import "../../blocks/intro-search-panel/intro-search-panel";
 import "../../blocks/room-item/room-item";
 import "../../blocks/datepicker/datepicker";
-
+import "../../blocks/book-room-panel/book-room-panel";
